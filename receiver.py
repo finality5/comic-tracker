370a24bff9c97da3d52aa236bc8c1513506eac32
wattanai sathuphan <wattanai.sat@ku.th>
2019-11-18 18:47:43 +0700

    print publish date

--- receiver.py
+++ receiver.py
@@ -6,13 +6,15 @@ comic_list = f.read().split('\n')[:-1]
 f.close()
 for url in comic_list:
     try:
+        time.sleep(1)
         res = requests.get(url).text
         raw_res = res.split('<a class="lst"')
         raw_last_chap = raw_res[1].split('\n')[0].split('"')
-        print(raw_last_chap[3], raw_last_chap[1])
+        print(raw_last_chap[3])
+        print(raw_last_chap[1])
+        print(raw_res[1].split('\n')[3].split('"')[3])
+        print()
     except:
         print("Error . . Invalid link? No internet connection?")
-    print()
-    time.sleep(1)
 
     
